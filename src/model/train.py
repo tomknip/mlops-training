7d9ab9d83a385e8cf3aec872fbf7c58fe2b18ab8
Tom van Knippenberg <tomvanknippenberg@hotmail.com>
2022-12-20 12:48:57 +0100

    Update styling flake8

--- src/model/train.py
+++ src/model/train.py
@@ -73,7 +73,7 @@ def parse_args():
 
     # add arguments
     parser.add_argument("--training_data", dest="training_data", type=str)
-    parser.add_argument("--reg_rate", dest="reg_rate", type=float, 
+    parser.add_argument("--reg_rate", dest="reg_rate", type=float,
                         default=0.01)
 
     # parse args
